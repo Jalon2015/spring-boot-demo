5dc7a1d34a68cdc0485079f93fa209640f0a1aef
jalon2015 <1121263265@qq.com>
2021-06-03 14:45:11 +0800

    form-login

--- demo-spring-security/demo-spring-security-basic-auth/README.md
+++ demo-spring-security/demo-spring-security-basic-auth/README.md
@@ -1,3 +1,6 @@
 ## 简介
-Spring Security的用户名密码认证：basic auth方式
+用户名密码认证：http basic auth方式
 适用于前后端分离的项目，比如前端vue，后端rest
+## 目录介绍
+web: vue-cli例子，只有一个主页按钮，用来发起请求
+src: 后台http认证接口，接收前端传来的请求（需提供username/password进行认证)
--- demo-spring-security/demo-spring-security-login-form/src/main/java/com/jalon/security/controller/SecurityController.java
+++ demo-spring-security/demo-spring-security-login-form/src/main/java/com/jalon/security/controller/SecurityController.java
@@ -33,15 +33,6 @@ public class SecurityController {
         return "login";
     }
 
-    @RequestMapping("/loginout")
-    public String loginout(HttpServletRequest request, HttpServletResponse response){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(authentication!=null){
-            new SecurityContextLogoutHandler().logout(request, response, authentication);
-        }
-        return "redirect: /login?logout";
-    }
-
     @RequestMapping("/home")
     public String home(Model model){
         model.addAttribute("user", getUsername());
