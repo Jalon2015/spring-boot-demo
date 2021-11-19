ae25a3c93eb115f8cc7877d7eeeba53921d3f3de
jalon2015 <1121263265@qq.com>
2021-11-19 12:38:24 +0800

    spring-security-redirect

---
+++ demo-spring-security/demo-spring-security-login-redirect/src/main/java/com/jalon/security/redirect/config/SecurityConfiguration.java
@@ -0,0 +1,80 @@
+package com.jalon.security.redirect.config;
+
+import com.jalon.security.redirect.handler.MySimpleUrlAuthenticationSuccessHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+/**
+ * <p>
+ *  安全配置类
+ * </p>
+ *
+ * @author: JavaLover
+ * @date: 2021/4/12 17:39
+ */
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler(){
+        return new MySimpleUrlAuthenticationSuccessHandler();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // 数据没有持久化，只是保存在内存中
+        auth.inMemoryAuthentication()
+                .withUser("javalover").password(passwordEncoder().encode("123456")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("123456")).roles("ADMIN");
+    }
+
+    // 授权相关操作
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        log.info("=== SecurityConfiguration.authorize ===");
+        http
+            // 关闭csrf，此时登出logout接收任何形式的请求；（默认开启，logout只接受post请求）
+            .csrf().disable()
+            .authorizeRequests()
+            // admin页面，只有admin角色可以访问
+            .antMatchers("/admin").hasRole("ADMIN")
+            // home 页面，ADMIN 和 USER 都可以访问
+            .antMatchers("/home").hasAnyRole("USER", "ADMIN")
+            // login 页面，所有用户都可以访问
+            .antMatchers("/login").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            // 自定义登录表单
+            .formLogin().loginPage("/login")
+            // 登录成功跳转的页面，第二个参数true表示每次登录成功都是跳转到home，如果false则表示跳转到登录之前访问的页面
+//            .defaultSuccessUrl("/home", true)
+                .successHandler(authenticationSuccessHandler())
+            // 失败跳转的页面（比如用户名/密码错误），这里还是跳转到login页面，只是给出错误提示
+            .failureUrl("/login?error=true")
+            .and()
+            // 登出 所有用户都可以访问
+            .logout().permitAll()
+            .and()
+            // 权限不足时跳转的页面，即访问一个页面时没有对应的权限，会跳转到这个页面
+            .exceptionHandling().accessDeniedPage("/accessDenied");
+    }
+
+    // 定义一个密码加密器，这个BCrypt也是Spring默认的加密器
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ demo-spring-security/demo-spring-security-login-redirect/src/main/java/com/jalon/security/redirect/handler/MySimpleUrlAuthenticationSuccessHandler.java
@@ -0,0 +1,67 @@
+package com.jalon.security.redirect.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author: syj
+ * @date: 2021/11/18
+ */
+public class MySimpleUrlAuthenticationSuccessHandler
+        implements AuthenticationSuccessHandler {
+
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response, Authentication authentication)
+            throws IOException {
+        String targetUrl = determineTargetUrl(authentication);
+
+        if (response.isCommitted()) {
+            System.out.println("Response has already been committed. Unable to redirect to "
+                    + targetUrl);
+            return;
+        }
+
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+
+    }
+
+
+
+    protected String determineTargetUrl(final Authentication authentication) {
+
+        Map<String, String> roleTargetUrlMap = new HashMap<>();
+        roleTargetUrlMap.put("ROLE_USER", "/home");
+        roleTargetUrlMap.put("ROLE_ADMIN", "/admin");
+
+        final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (final GrantedAuthority grantedAuthority : authorities) {
+            String authorityName = grantedAuthority.getAuthority();
+            if(roleTargetUrlMap.containsKey(authorityName)) {
+                return roleTargetUrlMap.get(authorityName);
+            }
+        }
+
+        throw new IllegalStateException();
+    }
+
+}
