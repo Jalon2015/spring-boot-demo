2d2b7eadeefe3913c7d9ebba2e6eb2e94d921ee9
jalon2015 <1121263265@qq.com>
2021-11-22 16:26:42 +0800

    spring security remember me

---
+++ demo-spring-security/demo-spring-security-remember-me/src/main/java/com/jalon/security/SecurityRememberMeApplication.java
@@ -0,0 +1,19 @@
+package com.jalon.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author: JavaLover
+ * @date: 2021/4/12 18:28
+ */
+@SpringBootApplication
+public class SecurityRememberMeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityRememberMeApplication.class, args);
+    }
+}
