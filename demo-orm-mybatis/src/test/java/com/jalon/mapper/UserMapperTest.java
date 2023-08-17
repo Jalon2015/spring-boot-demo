96a3ee57b21af0d8b2566dc460c6236658cfd104
jalon <1121263265@qq.com>
2023-08-17 09:33:22 +0800

    update:mybatis增加test

--- demo-orm-mybatis/src/test/java/com/jalon/mapper/UserMapperTest.java
+++ demo-orm-mybatis/src/test/java/com/jalon/mapper/UserMapperTest.java
@@ -1,9 +1,22 @@
 package com.jalon.mapper;
 
 
+import com.jalon.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
 class UserMapperTest {
 
-
+    @Autowired
+    UserMapper userMapper;
+    @Test
     void selectAllUser() {
+        List<User> userList = userMapper.selectAllUser("ja2");
+        Assertions.assertFalse(userList.isEmpty(), "列表为空");
     }
 }
